fb64e647c637e2c6f1b8e45f0707a552e061b1ab
sh1kasi <elfarrel07@gmail.com>
2025-06-24 02:32:23 +0700

    add excerpt to product cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $products = Product::get();
+        $categories = Category::get();
 
-        return view('products');
-        
+        return view('products', compact('products', 'categories'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Middleware\isAdmin;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('/register/province', [RegisterController::class, 'province'])->name('register.province');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])->middleware('auth')->name('product.index');
+
+Route::post('/cart', [CartController::class, 'addToCart'])->middleware('auth')->name("cart.addToCart");
