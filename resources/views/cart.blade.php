f588f22367d589f2502cd28e7dbf33c2b80167ab
BagasApg <bagasapg@gmail.com>
2025-06-24 10:31:34 +0700

    Added checkout button

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -9,22 +9,31 @@
         </div>
         <div class="row">
             <div class="col-md-9 mx-auto fs-5">
-                <table class="table table-bordered border-black bg-white">
-                    <tr>
+                <table class="table table-bordered border-black bg-white mb-4">
+                    <tr class="text-center">
                         <th>No</th>
                         <th>Nama Produk dengan ID</th>
                         <th>Jumlah</th>
                         <th>Harga</th>
                     </tr>
+
+                    {{-- Loop here --}}
                     <tr>
-                        <td>1</td>
+                        <td class="text-center">1</td>
                         <td>Thermometer 10291</td>
-                        <td>1</td>
+                        <td class="text-center">1</td>
                         <td>@currency(250000)</td>
                     </tr>
+                    {{-- Loop here --}}
+
                 </table>
-                <p class="fs-5">Total Belanja (termasuk pajak): <span class="text-decoration-underline">
-                        @currency(250000)</span></p>
+                <div class="d-flex justify-content-between">
+                    <p class="fs-5 m-0">Total Belanja (termasuk pajak): <span class="text-decoration-underline">
+                            @currency(250000)</span></p>
+                    <a href="">
+                        <div class="btn btn-success">Checkout</div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
